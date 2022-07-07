226bf48c324a0933509cdaa7d8c269382be09f99
ASmallPineTree <sy61520091@163.com>
2022-07-07 15:05:51 +0800

    环境搭建

---
+++ shop-common/src/main/java/entity/User.java
@@ -0,0 +1,36 @@
+package entity;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ * 用户
+ *
+ * @author sy615
+ * @date 2022/07/07
+ */
+@Entity(name = "shop_user")
+@Data
+public class User {
+    /**
+     * 主键
+     */
+    @Id
+    @GeneratedValue
+    private Integer uid;
+    /**
+     * 用户名
+     */
+    private String username;
+    /**
+     * 密码
+     */
+    private String password;
+    /**
+     * 电话
+     */
+    private String telephone;
+}
